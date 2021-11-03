e8a37fd91310267d4023856bb8badc1d493e07e8
Kelvin Chung <kelvinchung97@gmail.com>
2021-11-03 10:26:41 -0700

    C2 first commit

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,75 +1,33 @@
-import { parse as p5} from "parse5";
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import * as k from "./UtilsK";
+import * as k2 from "./UtilsK2";
 
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
  *
  */
-let dataSets: InsightDataset[];
-let allSections: any[] = [];
-let allParsedRooms: any[] = [];
+let dataSets: InsightDataset[] = [];
+
+// DO NOT USE THESE
+let buildingNames: string[] = [];
+let shortNames: string[] = [];
+let allBuildings: any[] = [];
+// DO NOT USE THESE
 export default class InsightFacade implements IInsightFacade {
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
-		dataSets = [];
-		let fs = require("fs");
-		// https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
-		if (!fs.existsSync("./src/data")) {
-			fs.mkdirSync("./src/data");
-		}
-		let diskDatasets = fs.readdirSync("./src/data");
-		diskDatasets.forEach((disk: any) => {
-			let read = fs.readFileSync(`./src/data/${disk}`).toString("utf8");
-			// https://stackoverflow.com/questions/48676751/convert-javascript-string-to-literal-array
-			dataSets.push(
-				{
-					id: disk.split(".")[0],
-					kind: InsightDatasetKind.Courses,
-					numRows: JSON.parse(read.replace(/'/g, '"')).length
-				}
-			);
-		});
+		k2.init(dataSets);
 	}
 
-	/**
-	 * Add a dataset to insightUBC.
-	 *
-	 * @param id  The id of the dataset being added. Follows the format /^[^_]+$/
-	 * @param content  The base64 content of the dataset. This content should be in the form of a serialized zip file.
-	 * @param kind  The kind of the dataset
-	 *
-	 * @return Promise <string[]>
-	 *
-	 * The promise should fulfill on a successful add, reject for any failures.
-	 * The promise should fulfill with a string array,
-	 * containing the ids of all currently added datasets upon a successful add.
-	 * The promise should reject with an InsightError describing the error.
-	 *
-	 * An id is invalid if it contains an underscore, or is only whitespace characters.
-	 * If id is the same as the id of an already added dataset, the dataset should be rejected and not saved.
-	 *
-	 * After receiving the dataset, it should be processed into a data structure of
-	 * your design. The processed data structure should be persisted to disk; your
-	 * system should be able to load this persisted value into memory for answering
-	 * queries.
-	 *
-	 * Ultimately, a dataset must be added or loaded from disk before queries can
-	 * be successfully answered.
-	 */
-
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		allSections = [];
 		let numRows: number;
-		let fs = require("fs");
 		let JSZip = require("jszip");
 		let zip = new JSZip();
 		let addedCourses: any[] = [];
-		if (this.checkAddId(id)) { // is valid
+		if (k.checkAddId(id, dataSets)) { // is valid
 			return zip.loadAsync(content, {base64: true}).then((dataset: any) => {
-				// numRows = Object.keys(dataset.files).length;
 				if (kind === InsightDatasetKind.Courses) {
-					// Need to check what happens if courses is not a folder
 					let allFiles: any = dataset.folder("courses")["files"];
 					let allKeys = Object.keys(allFiles);
 					numRows = allKeys.length - 1;
@@ -82,27 +40,22 @@ export default class InsightFacade implements IInsightFacade {
 						}
 					}
 					try {
-						return this.readCourses(addedCourses);
+						return k.readCourses(addedCourses);
 					} catch {
 						return Promise.reject(new InsightError("Could not read"));
 					}
 				} else {
-					this.readRooms(dataset);
+					return this.getBuildingsFromIndex(dataset).then((buildings: any[]) => {
+						return Promise.allSettled(k2.getGets(allBuildings, buildings, shortNames, buildingNames));
+					}).then((addresses: any) => {
+						return k2.getAllRoomData(addresses);
+					}).catch ((error: any) => {
+						return Promise.reject(error);
+					});
 				}
 			}).then(
 				(success: any) => {
-					// https://stackoverflow.com/questions/51577849/how-to-save-an-array-of-strings-to-a-json-file-in-javascript
-					// https://stackoverflow.com/questions/12899061/creating-a-file-only-if-it-doesnt-exist-in-node-js
-					fs.writeFile(`./src/data/${id}.json`, JSON.stringify(allSections),
-						{flag: "wx"}, function (error: any) {
-							if (error) {
-								return Promise.reject(new InsightError("Could not write"));
-							}
-						});
-					this.confirmAddDataset(id, kind, numRows);
-					return Promise.resolve(dataSets.map((dataset) => {
-						return dataset.id;
-					}));
+					return k2.succesful(success, id, kind, numRows, dataSets);
 				},
 				function(error: any) {
 					return Promise.reject(new InsightError(error));
@@ -112,105 +65,37 @@ export default class InsightFacade implements IInsightFacade {
 		return Promise.reject(new InsightError("ID Error"));
 	}
 
-	private readRooms(dataset: any): Promise<void | any[]> {
+	private getBuildingsFromIndex(dataset: any): Promise<any[]> {
 		let parse5 = require("parse5");
 		let allRooms: any = dataset.folder("rooms")["files"];
 		let allKeys: any = Object.keys(allRooms);
 		let files: string[] = allKeys.filter((key: string) => key[key.length - 1] !== "/");
 		if (!files.includes("rooms/index.htm")) {
-			console.log("no index file");
 			return Promise.reject(new InsightError("No index file"));
 		}
-		// numRows = allKeys.length - 1;
-		let addedRooms: any[] = [];
-		for (const key of allKeys) {
-			if (key !== "rooms/index.htm") {
-				addedRooms.push(allRooms[key].async("string"));
-			}
-		}
-		console.log("entering promise");
-		return Promise.all(addedRooms).then((rooms) => {
-			for (const room in rooms) {
-				let parsed: any = parse5.parse(room);
-				allParsedRooms.push(parsed); // Need to parse the room, check the site
-			}
-			return allParsedRooms;
-		}).then((stuff) => {
-			console.log(stuff);
-		});
-	}
-
-	private confirmAddDataset(id: string, kind: InsightDatasetKind, numRows: number) {
-		let newDataset = {
-			id: id,
-			kind: kind,
-			numRows: numRows
-		};
-		dataSets.push(newDataset); // Add new dataset
-	}
-
-	private readCourses(addedCourses: any[]): Promise<void | any[]> {
-		return Promise.all(addedCourses).then((courses) => {
-			let empty: boolean = true;
-			courses.forEach((course) => {
-				let result: any[];
-				result =  JSON.parse(course)["result"]; // this is a list of sections
-				if (result.length > 0) {
-					empty = false;
+		return allRooms["rooms/index.htm"].async("string").then((data: any) => {
+			let indexes: any[] = [];
+			try {
+				let parsed: any = parse5.parse(data);
+				let child = k.returnToDiv(parsed);
+				if (child) {
+					let tbody = k2.getTBody(child);
+					buildingNames = [];
+					shortNames = [];
+					k2.getInfo(tbody, indexes, buildingNames, shortNames);
 				}
-				for (const i in result) {
-					let section = result[i];
-					let thisSection: any = {
-						dept: section["Subject"],
-						id: section["Course"],
-						avg: section["Avg"],
-						instructor: section["Professor"],
-						title: section["Title"],
-						pass: section["Pass"],
-						fail: section["Fail"],
-						audit: section["Audit"],
-						uuid: section["id"],
-						year: section["Year"]
-					};
-					allSections.push(thisSection);
-				}
+			} catch {
+				return Promise.reject(new InsightError("Failed to parse"));
+			}
+			return indexes;
+
+		}).then((indexes: any[]) => {
+			indexes = indexes.map((index: string) => index.replace(".", "rooms"));
+			let addedRooms = indexes.map((index: string) => {
+				return allRooms[index].async("string");
 			});
-			if (empty) {
-				return Promise.reject(new InsightError("Empty dataset"));
-			}
+			return Promise.all(addedRooms);
 		});
-	}
-	// https://stackoverflow.com/questions/10261986/
-	// how-to-detect-string-which-only-contains-white-spaces/50971250 code for whitespaces
-
-	private checkAddId(id: string): boolean {
-		if (!id) {
-			// throw new InsightError();
-			return false;
-		}
-		if (id.includes("_")) {
-			return false;
-			// throw new InsightError();
-		}
-		if (!id.replace(/\s/g, "").length) {
-			return false;
-			// throw new InsightError();
-		}
-		if (this.isDatasetsContainsId(id)) {
-			return false;
-			// throw new InsightError();
-		}
-		return true;
-	}
-
-	private isDatasetsContainsId(id: string) {
-		let isContains: boolean = false;
-		dataSets.forEach((element) => {
-			if (element.id === id) {
-				isContains = true;
-			}
-		});
-		return isContains;
 	}
 
 	public removeDataset(id: string): Promise<string> {
@@ -223,14 +108,5 @@ export default class InsightFacade implements IInsightFacade {
 
 	public listDatasets(): Promise<InsightDataset[]> {
 		return Promise.resolve(dataSets);
-	}
-
-	private includes(element: any, array: any[]): boolean {
-		array.forEach((item) => {
-			if (element === item) {
-				return true;
-			}
-		});
-		return false;
 	}
 }
---
+++ src/controller/UtilsK2.ts
@@ -0,0 +1,121 @@
+import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
+import { parse as p5} from "parse5";
+import * as k from "./UtilsK";
+import { inherits } from "util";
+
+
+export function getTBody(child: any) {
+	child = k.returnChild(child, "div", "class", "view-content");
+	child = k.returnChild(child, "table");
+	let tbody = k.returnChild(child, "tbody");
+	return tbody.filter((obj: any) => obj["nodeName"] && obj["nodeName"] === "tr");
+}
+
+export function getInfo(tbody: any, indexes: any[], buildingNames: any[], shortNames: any[]) {
+	for (const tr of tbody) {
+		if (!tr["childNodes"]) {
+			continue;
+		}
+		let tds: any[] = tr["childNodes"];
+		tds = tds.filter((obj: any) => obj["nodeName"] && obj["nodeName"] === "td");
+		if (tds.length === 5) {
+			indexes.push(k.makeIndex(tds[4]));
+			let td2 = tds[2];
+			if (td2["childNodes"]) {
+				for (const tdChild of td2["childNodes"]) {
+					if (k.isBName(tdChild)) {
+						buildingNames.push(tdChild["childNodes"][0]["value"]);
+						break;
+					}
+				}
+			}
+			let td1 = tds[1];
+			if (td1["childNodes"]) {
+				for (const tdChild of td1["childNodes"]) {
+					if (tdChild["nodeName"] && tdChild["nodeName"] === "#text") {
+						if (tdChild["value"]) {
+							// https://stackoverflow.com/questions/9364400/remove-not-alphanumeric-characters-from-string
+							shortNames.push(tdChild["value"].replace(/\W/g, ""));
+							break;
+						}
+					}
+				}
+			}
+
+		} else {
+			return Promise.reject(new InsightError("Failed to parse"));
+		}
+	}
+}
+
+export function init(dataSets: any[]) {
+	let fs = require("fs");
+	// https://stackoverflow.com/questions/21194934/how-to-create-a-directory-if-it-doesnt-exist-using-node-js
+	if (!fs.existsSync("./src/data")) {
+		fs.mkdirSync("./src/data");
+	}
+	let diskDatasets = fs.readdirSync("./src/data");
+	diskDatasets.forEach((disk: any) => {
+		let read = fs.readFileSync(`./src/data/${disk}`).toString("utf8");
+		// https://stackoverflow.com/questions/48676751/convert-javascript-string-to-literal-array
+		dataSets.push(
+			{
+				id: disk.split(".")[0],
+				kind: InsightDatasetKind.Courses,
+				numRows: JSON.parse(read.replace(/'/g, '"')).length
+			}
+		);
+	});
+}
+
+export function succesful(success: any, id: string, kind: InsightDatasetKind, numRows: number, dataSets: any[]) {
+	// https://stackoverflow.com/questions/51577849/how-to-save-an-array-of-strings-to-a-json-file-in-javascript
+	// https://stackoverflow.com/questions/12899061/creating-a-file-only-if-it-doesnt-exist-in-node-js
+	let fs = require("fs");
+	fs.writeFile(`./src/data/${id}.json`, JSON.stringify(success),
+		{flag: "wx"}, function (error: any) {
+			if (error) {
+				return Promise.reject(new InsightError("Could not write"));
+			}
+		});
+	k.confirmAddDataset(id, kind, numRows, dataSets);
+	return Promise.resolve(dataSets.map((dataset) => dataset.id));
+}
+
+export function getGets(allBuildings: any[], buildings: any[], shortNames: string[], buildingNames: string[]) {
+	let parse5 = require("parse5");
+	allBuildings = buildings.map((building: any) => parse5.parse(building));
+	let awaiting: any[] = [];
+	let temp1: string[] = [];
+	let temp2: string[] = [];
+	for (let i = 0; i < allBuildings.length; i++) {
+		try {
+			temp1.push(k.getBName(allBuildings[i]));
+			temp2.push(shortNames[buildingNames.indexOf(temp1[i])]);
+		} catch {
+			throw new InsightError("Building Names do not match");
+		}
+	}
+	buildingNames = temp1;
+	shortNames = temp2;
+	for (let i = 0; i < allBuildings.length; i++) {
+		let addr: string = k.getAddress(allBuildings[i]);
+		awaiting.push(k.httpGet(addr, buildingNames[i], shortNames[i], allBuildings[i]));
+	}
+	return awaiting;
+}
+
+export function getAllRoomData(addresses: any): any {
+	addresses = addresses.filter((obj: any) => obj["status"] === "fulfilled");
+	// List of list of LOC, PARSE, Name, Short, Addr
+	let validBuildings = addresses.map((obj: any) => obj["value"]);
+	let allRoomData: any[] = [];
+	for (const building of validBuildings) {
+		allRoomData = allRoomData.concat(k.parseBuilding(building[1],
+			building[2], building[3], building[4], building[0]));
+	}
+	if (!allRoomData) {
+		return Promise.reject(new InsightError("No rooms"));
+	}
+	return allRoomData;
+}
